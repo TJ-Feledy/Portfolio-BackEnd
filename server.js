fe29294dfdf4dd77c81817acf4012c13cb64f32d
TJ-Feledy <teej387@yahoo.com>
2020-07-07 20:46:39 -0500

    added twilio and created endpoint for texting

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
+const client = require('twilio')(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+)
 
 require('dotenv').config()
 
@@ -9,11 +14,30 @@ const server = express()
 
 server.use(express.json())
 server.use(cors())
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
 
 server.use('/api/count', countRouter)
 
 server.get('/', (req, res) => {
     res.send('Can you see it?')
+})
+
+server.post('/text', (req, res) => {
+    res.header('Content-Type', 'application/json')
+    client.messages
+        .create({
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: +12195772978,
+            body: req.body.body
+        })
+        .then(() => {
+            res.send(JSON.stringify({ success: true }))
+        })
+        .catch((err) => {
+            console.log(err)
+            res.send(JSON.stringify({ success: false }))
+        })
 })
 
 module.exports = server
